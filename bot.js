b179909eebe44a39e0148c515c46322e36f2a5b9
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-11-27 21:08:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "*";
 var adminprefix = '*'
-const developers = ["355126496169426945"]
+const developers = ["516682726951092235"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
